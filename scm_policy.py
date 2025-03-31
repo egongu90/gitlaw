20e182656d7c12096b1c0a19262d4a0c725d6077
xNaaro <13362103+xNaaro@users.noreply.github.com>
2025-03-31 20:30:30 +0200

    Gitlab application/settings working (#6)
    * Gitlab application/settings working
    
    * Add depenencies to requirements

--- scm_policy.py
+++ scm_policy.py
@@ -16,20 +16,21 @@ try:
     SERVER_AUTH_TOKEN = os.environ['SERVER_AUTH_TOKEN']
 except KeyError as e:
     raise KeyError(f'Environment variable {e} not found') from e
-CONFIG_FILE = 'minimum.yml'
+CONFIG_FILE = 'examples/example.yml'
 
 def main():
     """Main method."""
 
     config_data = Yamlloader().read_file(CONFIG_FILE)
-    print("Setting service config")
-    BackendManager(config_data.get('organization')).handle_backend_type(SERVER_URL,
-                                                                        SERVER_AUTH_TOKEN)
 
     # Just for testing to read full rendered file
     # import yaml
     # with open('/tmp/dumped.yaml', 'w') as file:
     #     yaml.dump(config_data, file)
+
+    print("Setting service config")
+    BackendManager(config_data.get('organization')).handle_backend_type(SERVER_URL,
+                                                                        SERVER_AUTH_TOKEN)
 
 if __name__=="__main__":
     main()
--- scm_policy/gitlab/backend.py
+++ scm_policy/gitlab/backend.py
@@ -1,15 +1,37 @@
+"""Module to orchestrate gitlab interactions."""
 import gitlab
 
 from scm_policy.gitlab.settings import Settings
 
-class Gitlab:
-    def __init__(self, config, server_url, server_auth_token):
+class GitlabBackend:
+    """Gitlab class.
+
+    Manages interactions with GitLab APIs.
+    """
+    def __init__(self, config, server_url, server_auth_token) -> None:
+        """Init class.
+        
+        Args:
+        config: Config data.
+        server_url: GitLab server URL.
+        server_auth_token: Gitlab auth token
+
+        Returns: None
+        """
         self.config = config
         self.gl = self.handle_auth(server_url, server_auth_token)
 
-    def entrypoint(self):
+    def entrypoint(self) -> None:
+        """Call gitlab methods."""
         Settings(self.gl, self.config.get('service')).manager()
 
-    def handle_auth(self, server_url, server_auth_token):
+    def handle_auth(self, server_url, server_auth_token) -> object:
+        """Init gitlab object with auth.
+
+        Args:
+        server_url: GitLab server URL.
+        server_auth_token: Gitlab auth token
+        ssl_verify: Verify SSL connections, defaults to False
+        """
         gl = gitlab.Gitlab(url=server_url, private_token=server_auth_token, ssl_verify=False)
         return gl
