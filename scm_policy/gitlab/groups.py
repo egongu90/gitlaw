7760c0047fa7e0006d2c7f0db265d1ca2d2f26b3
xNaaro <13362103+xNaaro@users.noreply.github.com>
2025-04-03 20:35:13 +0200

    Add partial projects support (#8)

--- scm_policy/gitlab/groups.py
+++ scm_policy/gitlab/groups.py
@@ -29,7 +29,20 @@ class Groups:
         self.eval_changes(user_group, server_object)
         print(f"Configuring members of group {self.config.get('name', None)}...")
         self.configure_members(members=self.config.get('members', None), server_obj=server_object)
+        srv_projects = self.list_projects(server_object)
+        print(f"Configuring projects for group {self.config.get('name', None)}...")
 
+        # Iterate over server projects, if project is not in user defined list,
+        # adds the project into the user list for default configuration.
+        for srv in srv_projects:
+            if not any(x.get('name') == srv.name for x in self.config.get('projects', [])):
+                self.config.get('projects').append({'name': srv.name})
+
+        # With the full list of projects configure each one
+        for project in self.config.get('projects', []):
+            project_defaults = self._set_project_defaults(project.get('name'), policy=project.get('policy', {}))
+            print(f"Configuring project {project.get('name')}...")
+            self.eval_project_changes(project_defaults)
     def get_groups(self, auto_create_groups) -> dict:
         """Query config data from the API.
 
@@ -119,3 +132,70 @@ class Groups:
                                             'access_level': member.get('access_level')})
         except IndexError as exc:
             raise IndexError(f"User {member.get('name')} does not exists") from exc
+
+    def list_projects(self, srv_object):
+        """List projects of a group."""
+        projects = srv_object.projects.list()
+        return projects
+
+
+    def _set_project_defaults(self, name, policy):
+        """Set default values if not defined in the yaml config file.
+        
+        Args:
+        name: Group name
+        description: Group description
+        policy: Group policy config data
+        """
+        defaults = {}
+        defaults['name'] = name
+        defaults['visibility'] = policy.get('visibility', "private")
+        defaults['git_strategy'] = policy.get('git_strategy', "fetch")
+        defaults['build_timeout'] = policy.get('build_timeout', 3600)
+        defaults['ci_config_path'] = policy.get('ci_config_path', None)
+        defaults['group_runners_enabled'] = policy.get('group_runners_enabled', True)
+        defaults['issues_template'] = policy.get('issues_template', None)
+        defaults['merge_commit_template'] = policy.get('merge_commit_template', None)
+        defaults['max_artifacts_size'] = policy.get('max_artifacts_size', None)
+        defaults['lfs'] = policy.get('lfs', True)
+        defaults['merge_method'] = policy.get('merge_method', "merge")
+        defaults['merge_pipelines_enabled'] = policy.get('merge_pipelines_enabled', True)
+        defaults['merge_trains_enabled'] = policy.get('merge_trains_enabled', True)
+        defaults['only_allow_merge_if_all_discussions_are_resolved'] = \
+            policy.get('only_allow_merge_if_all_discussions_are_resolved', False)
+        defaults['only_allow_merge_if_pipeline_succeeds'] = policy.get('only_allow_merge_if_pipeline_succeeds', False)
+        defaults['packages_enabled'] = policy.get('packages_enabled', True)
+        defaults['public_jobs'] = policy.get('public_jobs', True)
+        defaults['remove_source_branch_after_merge'] = policy.get('remove_source_branch_after_merge', True)
+        defaults['request_access_enabled'] = policy.get('request_access_enabled', True)
+        defaults['resolve_outdated_diff_discussions'] = policy.get('resolve_outdated_diff_discussions', False)
+        defaults['shared_runners_enabled'] = policy.get('shared_runners_enabled', True)
+        defaults['squash_option'] = policy.get('squash_option', "default_off")
+        # defaults['visibility'] = policy.get('visibility', "private")
+        # defaults['visibility'] = policy.get('visibility', "private")
+        # defaults['visibility'] = policy.get('visibility', "private")
+        # defaults['visibility'] = policy.get('visibility', "private")
+        # defaults['visibility'] = policy.get('visibility', "private")
+        # defaults['visibility'] = policy.get('visibility', "private")
+        return defaults
+
+
+    def eval_project_changes(self, user_project):
+        """Checks for changes on group api.
+
+        Evaluate if the config file data provided match with data in GitLab group API,
+        if does not match, set value with the user data and store changes in the API"""
+        project_id = self.gl.projects.list(search=user_project.get('name'))[0].id
+        project = self.gl.projects.get(project_id)
+        for key, value in user_project.items():
+            try:
+                # Some user provided data might me None and continuing the loop will fail.
+                if value is not None and getattr(project, key) != value:
+                    print(f"Expected value in project {project.name} `{key}: {value}` does not match with remote "
+                          f"`{getattr(project, key)}`")
+                    setattr(project, key, value)
+                    project.save()
+            except AttributeError:
+                # When some attribute is only present in licensed servers, the attribute
+                # does not exists in the API and raises this exception.
+                pass
